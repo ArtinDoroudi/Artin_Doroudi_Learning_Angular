afe4f0f46acecbe421106ba085191e26065a0e45
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-09 23:53:22 -0400

    implemented app.component.ts and made an array with 6 instances and implemented app.component.html (also added some css)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,53 @@
+import { Component } from '@angular/core';  // You need to import Component
+import { Post } from './shared/models/post';  // Import the Post interface
+import {JsonPipe, NgForOf} from "@angular/common";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, NgForOf, JsonPipe],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
 export class AppComponent {
-  title = 'Artin_Doroudi_Learning_Angular';
-  name: string = 'Artin';
-  age: number = 25;
+  posts: Post[] = [
+    {
+      id: 1,
+      title: 'Good Morning, Night City!',
+      content: 'This is a forum about Cyberpunk 2077.',
+      author: 'V',
+      isPinned: true
+    },
+    {
+      id: 2,
+      title: 'Second Post',
+      content: 'This is the second post.',
+      author: 'V'
+    },
+    {
+      id: 3,
+      title: 'Third Post',
+      content: 'This is the third post.',
+      author: 'V'
+    },
+    {
+      id: 4,
+      title: 'Fourth Post',
+      content: 'This is the fourth post.',
+      author: 'V'
+    },
+    {
+      id: 5,
+      title: 'Fifth Post',
+      content: 'This is the fifth post.',
+      author: 'V'
+    },
+    {
+      id: 6,
+      title: 'Am I a ghost?',
+      content: 'Or what?',
+      author: 'Johnny Silverhand'
+    }
+  ];
 }
