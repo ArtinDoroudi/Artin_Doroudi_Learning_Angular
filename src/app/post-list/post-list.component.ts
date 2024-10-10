1058062a2937f7e6b0bb0eece6c7deeb74f502cd
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 01:55:49 -0400

    added ngFor instead of showing them manually

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {PostListItemComponent} from "../post-list-item/post-list-item.component";
+import {NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-post-list',
   standalone: true,
   imports: [
-    PostListItemComponent
+    PostListItemComponent,
+    NgForOf
   ],
   templateUrl: './post-list.component.html',
   styleUrl: './post-list.component.css'
