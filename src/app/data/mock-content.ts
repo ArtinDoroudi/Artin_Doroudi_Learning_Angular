ff28c2261fc00a5e82c607a7111e5d523ac0d21e
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 10:55:33 -0400

    moved array from post-list.component.ts to data/mock-conent.ts and created post service

---
+++ src/app/data/mock-content.ts
@@ -0,0 +1,8 @@
+import { Post} from "../shared/models/post";
+
+export const POSTS: Post[] = [
+  { id: 1, title: 'Cyberpunk 2077 - Best Weapons', content: 'Discussion about the best weapons.', author: 'Johnny Silverhand' },
+  { id: 2, title: 'Exploring Night City', content: 'Share your best moments in Night City.', author: 'V' },
+  { id: 3, title: 'Game Tips and Tricks', content: 'Post your best tips for playing Cyberpunk 2077.', author: 'Panam' },
+  { id: 4, title: 'Favorite Side Quests', content: 'Discuss your favorite side quests.', author: 'Judy' }
+  ];
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -13,11 +13,5 @@ import {NgForOf} from "@angular/common";
   styleUrl: './post-list.component.css'
 })
 export class PostListComponent {
-  displayedColumns: string[] = ['id', 'title', 'content', 'author'];
-  posts = [
-    { id: 1, title: 'Cyberpunk 2077 - Best Weapons', content: 'Discussion about the best weapons.', author: 'Johnny Silverhand' },
-    { id: 2, title: 'Exploring Night City', content: 'Share your best moments in Night City.', author: 'V' },
-    { id: 3, title: 'Game Tips and Tricks', content: 'Post your best tips for playing Cyberpunk 2077.', author: 'Panam' },
-    { id: 4, title: 'Favorite Side Quests', content: 'Discuss your favorite side quests.', author: 'Judy' }
-  ];
+
 }
