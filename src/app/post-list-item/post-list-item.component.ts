bbc2ad0a03b512c2f38d83c6ae9065b5e9849d9d
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 02:01:10 -0400

    added odd/even cell style

--- src/app/post-list-item/post-list-item.component.ts
+++ src/app/post-list-item/post-list-item.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-post-list-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle
+  ],
   templateUrl: './post-list-item.component.html',
   styleUrl: './post-list-item.component.css'
 })
 export class PostListItemComponent {
   @Input() post: any;
+  @Input() isEven: boolean = false;
 }
