d3e77de0b897ca518898c851782d6789d8e4c839
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 11:59:05 -0400

    added single special post in AppComponent that use the service to retrieve a post with id

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,52 +3,27 @@ import { Post } from './shared/models/post';  // Import the Post interface
 import {JsonPipe, NgForOf} from "@angular/common";
 import { RouterOutlet } from '@angular/router';
 import {PostListComponent} from "./post-list/post-list.component";
+import {PostListItemComponent} from "./post-list-item/post-list-item.component";
+import {PostService} from "./post.service";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgForOf, JsonPipe, PostListComponent],
+  imports: [RouterOutlet, NgForOf, JsonPipe, PostListComponent, PostListItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  posts: Post[] = [
-    {
-      id: 1,
-      title: 'Good Morning, Night City!',
-      content: 'This is a forum about Cyberpunk 2077.',
-      author: 'V',
-      isPinned: true
-    },
-    {
-      id: 2,
-      title: 'Second Post',
-      content: 'This is the second post.',
-      author: 'V'
-    },
-    {
-      id: 3,
-      title: 'Third Post',
-      content: 'This is the third post.',
-      author: 'V'
-    },
-    {
-      id: 4,
-      title: 'Fourth Post',
-      content: 'This is the fourth post.',
-      author: 'V'
-    },
-    {
-      id: 5,
-      title: 'Fifth Post',
-      content: 'This is the fifth post.',
-      author: 'V'
-    },
-    {
-      id: 6,
-      title: 'Am I a ghost?',
-      content: 'Or what?',
-      author: 'Johnny Silverhand'
-    }
-  ];
+  specialPost: Post | undefined;
+  constructor(private postService: PostService) {}
+  ngOnInit(): void {
+    this.getSpecialPost(1);
+  }
+  getSpecialPost(id: number): void {
+    this.postService.getPostById(id).subscribe({
+      next: (data: Post | undefined) => this.specialPost = data,
+      error: err => console.error('Error fetching post by ID', err),
+      complete: () => console.log('Featured post fetch complete!')
+    });
+  }
 }
