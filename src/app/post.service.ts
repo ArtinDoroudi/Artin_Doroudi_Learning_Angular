53d8608f8341f4e2cbb324274ccdad48b4d9777b
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 11:34:11 -0400

    added CRUD

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -7,11 +7,39 @@ import { POSTS } from './data/mock-content';
   providedIn: 'root'
 })
 export class PostService {
+  private posts: Post[] = POSTS;
   constructor() { }
 
   getPosts(): Observable<Post[]> {
     return of(POSTS);
   }
 
+  // Adding CRUD methods
+  //Create: Add Post
+  addPost(newPost: Post): Observable<Post[]> {
+    this.posts.push(newPost);
+    return of(this.posts);
+  }
+
+  //Read: Get Post by ID
+  getPostById(postId: number): Observable<Post | undefined> {
+    const post = this.posts.find(post => post.id === postId);
+    return of(post);
+  }
+
+  //Update: Edit an existing Post
+  updatePost(updatedPost: Post): Observable<Post[]> {
+    const index = this.posts.findIndex(post => post.id === updatedPost.id);
+    if (index !== -1) {
+      this.posts[index] = updatedPost;
+    }
+    return of(this.posts);
+  }
+
+  //Delete: Remove a Post
+  deletePost(postId: number): Observable<Post[]> {
+    this.posts = this.posts.filter(post => post.id !== postId);  // remove the post with the given id
+    return of(this.posts);
+  }
 
 }
