c889e8783b726153428a1d8d101716f0f62ce8ee
Artin Doroudi <72512595+ArtinDoroudi@users.noreply.github.com>
2024-10-10 12:40:10 -0400

    created routes array and initilized two test values

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
+import { provideRouter, Routes } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/posts', pathMatch: 'full' },  // Default route redirects to posts list
+  { path: 'test', redirectTo: '/posts', pathMatch: 'full' },  // for test
+];
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
